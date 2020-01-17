ef430f4b68992fb0c5dfc28fa09d9801f89a7565
Eric Schulte <eschulte@grammatech.com>
2020-01-17 17:00:36 -0500

    Start adding fset integration

---
+++ fset.lisp
@@ -0,0 +1,85 @@
+(defpackage :functional-trees/fset
+  (:nicknames :ft/fset)
+  (:use cl :alexandria :iterate :functional-trees :fset)
+  (:shadowing-import-from :fset
+			  ;; Shadowed set operations
+			  #:union #:intersection #:set-difference #:complement
+			  ;; Shadowed sequence operations
+			  #:first #:last #:subseq #:reverse #:sort #:stable-sort
+			  #:reduce
+			  #:find #:find-if #:find-if-not
+			  #:count #:count-if #:count-if-not
+			  #:position #:position-if #:position-if-not
+			  #:remove #:remove-if #:remove-if-not
+			  #:substitute #:substitute-if #:substitute-if-not
+			  #:some #:every #:notany #:notevery
+                          ;; Shadowed from Alexandria
+                          #:compose #:unionf #:appendf #:removef)
+  (:shadowing-import-from :cl :set :map)
+  (:shadowing-import-from :iterate :with)
+  (:documentation "FSET Integration for functional-trees."))
+(in-package :functional-trees/fset)
+
+(defmacro do-tree ((var tree &key (path nil pathp)) &body body)
+  ;; ((var seq
+  ;;       &key (start nil start?) (end nil end?) (from-end? nil from-end??)
+  ;;       (index nil index?) (value nil))
+  ;;  &body body)
+  (with-gensyms (stack current)
+    `(let ((,stack (list ,tree)))
+       (iterate (for ,current = (pop ,stack))
+                (for ,var = (data ,current))
+                ,@(when pathp `((for ,path = (finger ,current))))
+                (while ,stack)
+                (collect ,@body)
+                (push (children ,current) ,stack)))))
+
+
+;;; Useful replacement function, not specific to FT or FSET.
+(defgeneric substitute-when (predicate sequence)
+  (:documentation
+   "Substitute elements of SEQUENCE with result of PREDICATE when non-nil.
+If secondary return value of PREDICATE is non-nil force substitution
+  with primary value even if it is nil.")
+  (:method (predicate (sequence sequence))
+    (let ((predicate (coerce predicate 'function)))
+      (map (type-of sequence)
+           (lambda (element)
+             (multiple-value-bind (value force)
+                 (funcall predicate element)
+               (if force value (or value element))))
+           sequence)))
+  (:method (predicate (seq seq) &aux result)
+    (let ((predicate (coerce predicate 'function)))
+      (do-seq (element seq)
+        (multiple-value-bind (value force)
+            (funcall predicate element)
+          (push (if force value (or value element)) result)))
+      (convert 'seq (nreverse result)))))
+
+
+;;; Relevant FSET operations for functional tree.
+(defmethod reduce (fn (node node) &rest rest &key &allow-other-keys)
+  (apply #'reduce fn (flatten (to-list node)) rest))
+
+(defmethod find (item (node node) &rest rest &key &allow-other-keys)
+  (apply #'find item (flatten (to-list node)) rest))
+
+(defmethod find-if (predicate (node node) &rest rest &key &allow-other-keys)
+  (apply #'find-if predicate (flatten (to-list node)) rest))
+
+(defmethod find-if-not (predicate (node node) &rest rest &key &allow-other-keys)
+  (apply #'find-if-not predicate (flatten (to-list node)) rest))
+
+(defmethod count (item (node node) &rest rest &key &allow-other-keys)
+  (apply #'count item (flatten (to-list node)) rest))
+
+(defmethod count-if (predicate (node node) &rest rest &key &allow-other-keys)
+  (apply #'count-if predicate (flatten (to-list node)) rest))
+
+(defmethod count-if-not (predicate (node node) &rest rest &key &allow-other-keys)
+  (apply #'count-if-not predicate (flatten (to-list node)) rest))
+
+(defmethod position (item (node node) &key (test #'equalp) &allow-other-keys)
+  (do-tree (current node :path location)
+    (when (funcall test item current) (return location))))
