e82f00335b053c09e7bf302427f59ddaf2331a72
Eric Schulte <eschulte@grammatech.com>
2020-01-21 11:29:11 -0500

    Fix includes

--- all.lisp
+++ all.lisp
@@ -1,3 +1,4 @@
 (uiop/package:define-package :functional-trees/all
-  (:nicknames :functional-trees :ft)
-  (:use-reexport :functional-trees/core :functional-trees/fset :fset))
+    (:nicknames :functional-trees :ft)
+  (:use-reexport :functional-trees/core :functional-trees/fset :fset)
+  (:shadowing-import-from :fset :some :every :notany :notevery))
