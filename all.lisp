537a193f03dea3527c39754692200316c004dffe
Eric Schulte <eschulte@grammatech.com>
2020-01-21 11:48:27 -0500

    Remove unnecessary #s

--- all.lisp
+++ all.lisp
@@ -17,13 +17,13 @@
                           :set :partition :alist :size
                           :range :compose :unionf :appendf :with :removef
 			  ;; Shadowed set operations
-			  #:union #:intersection #:set-difference #:complement
+			  :union :intersection :set-difference :complement
 			  ;; Shadowed sequence operations
-			  #:first #:last #:subseq #:reverse #:sort #:stable-sort
-			  #:reduce
-			  #:find #:find-if #:find-if-not
-			  #:count #:count-if #:count-if-not
-			  #:position #:position-if #:position-if-not
-			  #:remove #:remove-if #:remove-if-not
-			  #:substitute #:substitute-if #:substitute-if-not
-			  #:some #:every #:notany #:notevery))
+			  :first :last :subseq :reverse :sort :stable-sort
+			  :reduce
+			  :find :find-if :find-if-not
+			  :count :count-if :count-if-not
+			  :position :position-if :position-if-not
+			  :remove :remove-if :remove-if-not
+			  :substitute :substitute-if :substitute-if-not
+			  :some :every :notany :notevery))
