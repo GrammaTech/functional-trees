002eb6eefd7ef51b7d263e1c366b139da84392a8
Sam Estep <sestep@grammatech.com>
2020-06-04 13:36:25 -0400

    Add Python script to run Lisp examples from README
    I plan to soon add this script to the CI setup.
    
    GitLab [actually uses][1] the Ruby library [CommonMarker][2] to process
    Markdown files in repositories. Because CommonMark is standardized, I
    chose to instead use the Python CommonMark parser [Marko][3], because
    Python is one of the three languages we're using for this larger
    project. I chose Marko over the [other Python CommonMark parsers][4]
    because it seemed to most directly support GFM, which should correspond
    to the same set of extensions that GitLab uses via CommonMarker.
    
    I initially tried using the built-in Python [`subprocess`][5] module,
    but that [doesn't work well for multiple interactions with the REPL][6],
    so I chose to use the Pexpect library instead. Pexpect does have a
    specific [`replwrap`][7] module that looks like it should be perfect for
    this use case, but I wasn't able to get that to work immediately, so I
    just reverted to my working solution that uses `pexpect.spawn`.
    
    [1]: https://docs.gitlab.com/ee/user/markdown.html#transition-from-redcarpet-to-commonmark
    [2]: https://github.com/gjtorikian/commonmarker
    [3]: https://github.com/frostming/marko
    [4]: https://github.com/commonmark/commonmark-spec/wiki/List-of-CommonMark-Implementations#python
    [5]: https://stackoverflow.com/a/14458956
    [6]: https://stackoverflow.com/a/28616322
    [7]: https://pexpect.readthedocs.io/en/stable/api/replwrap.html

---
+++ readme.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+"""
+Check Lisp examples in README.md.
+
+To run:
+
+    pip3 install -r requirements.txt
+    ./readme.py
+
+The return code is zero iff all Lisp examples in the README run without
+errors in an SBCL REPL and their outputs match the given outputs. Such
+output can be specified in a language-less code block immediately
+following the Lisp code block.
+
+The whole REPL session is printed to stdout. If an error occurs, a
+Pexpect timeout exception is thrown. Otherwise, if output doesn't match,
+an Exception is thrown containing the input code, expected output, and
+actual output, respectively.
+"""
+
+import sys
+
+import marko.block as block
+import marko.ext.gfm as gfm
+import pexpect
+
+
+def pairwise(things):
+    """Return a list of pairs of adjacent elements from things.
+
+    The last element of this list is the pair (things[-1], None)."""
+    return zip(things, things[1:] + [None])
+
+
+def is_code_block(element):
+    """Return truthy iff the Marko element is a code block."""
+    types = [block.CodeBlock, block.FencedCode]
+    return any(isinstance(element, t) for t in types)
+
+
+def code_block_to_dict(code_block):
+    """Return a dict of the lang and text of the Marko code block."""
+    return {
+        'lang': code_block.lang,
+        # should only have one child but just in case; also, children of
+        # the child is just a string holding the text
+        'text': ''.join(child.children for child in code_block.children),
+    }
+
+
+def lisp_examples(element):
+    """Return a list of all Lisp examples in the Marko element.
+
+    A Lisp example is a code block whose language is 'lisp', and is
+    returned as a dictionary whose key 'code' holds the text of that
+    code block. If the Lisp code block is immediately followed by
+    another code block whose language is the empty string, then the text
+    of that second block is also included in the dictionary, under the
+    key 'output'."""
+    examples = []
+    if hasattr(element, 'children'):
+        children = element.children
+        # sometimes the children are just a string holding the text
+        if isinstance(children, list):
+            for a, b in pairwise(children):
+                if is_code_block(a):
+                    code = code_block_to_dict(a)
+                    if code['lang'] == 'lisp':
+                        example = {'code': code['text']}
+                        if is_code_block(b):
+                            output = code_block_to_dict(b)
+                            if not output['lang']:
+                                example['output'] = output['text']
+                        examples.append(example)
+                else:
+                    # will safely skip when a has no grandchildren
+                    examples.extend(lisp_examples(a))
+    return examples
+
+
+def slurp(filename):
+    """Return the contents of filename as a string."""
+    with open(filename) as file:
+        return file.read()
+
+
+examples = lisp_examples(gfm.gfm.parse(slurp('README.md')))
+# having echo would mean we have to strip the input code out of
+# repl.before; each code example must take less than one second, so that
+# when there's an error we only have to wait one second; encoding must
+# be set so that repl.before doesn't return binary strings
+repl = pexpect.spawn('sbcl', echo=False, timeout=1, encoding='utf-8')
+repl.logfile = sys.stdout
+# regex matching the default SBCL prompt
+prompt = r'\* '
+repl.expect(prompt)
+for example in examples:
+    code = example['code']
+    repl.send(code)
+    repl.expect(prompt)
+    if 'output' in example:
+        expected = example['output']
+        # Pexpect returns CR/LF
+        actual = repl.before.replace('\r\n', '\n')
+        if expected != actual:
+            raise Exception(code, expected, actual)
+repl.sendline('(cl-user::quit)')
