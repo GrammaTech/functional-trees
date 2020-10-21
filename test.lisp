6b6b2336a506168f93dfa721aa0f6bcec5172f5a
Eric Schulte <eschulte@grammatech.com>
2020-10-21 13:18:01 -0400

    Remove unnecessary insuite

--- test.lisp
+++ test.lisp
@@ -407,7 +407,6 @@ bucket getting at least 1.  Return as a list."
 ;;;; Test suite.
 (defroot test)
 (defsuite test "Functional trees top-level test suite.")
-(in-suite test)
 
 ;;; Simple Copy Tests
 (deftest simple-copy-tests ()
