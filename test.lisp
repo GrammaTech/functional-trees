d2225a9f08383f70bc3327f953f434e8ff798db5
Paul M. Rodriguez <prodriguez@grammatech.com>
2025-11-03 11:34:32 -0600

    Add test for do-child-slot-nodes

--- test.lisp
+++ test.lisp
@@ -2620,3 +2620,54 @@ a different caching policy."
     (signals ft/attrs:divergent-attribute
       (with-attr-table node
         (is (zerop (minimize node)))))))
+
+(deftest walk-child-slot-nodes ()
+  "Calling do-child-slot-nodes should return the children."
+  (is (equal
+       '(1 2)
+       (serapeum:with-collectors (collect)
+         (do-child-slot-nodes (c (make 'node-cons :head 1 :tail 2))
+           (collect c)))))
+  (is (equal
+       '(1 2 3)
+       (serapeum:with-collectors (collect)
+         (do-child-slot-nodes (c (make 'node-cons2 :head 1 :tail '(2 3)))
+           (collect c)))))
+  (is (equal
+       '(1 2 3)
+       (serapeum:with-collectors (collect)
+         (do-child-slot-nodes (c (make 'node-list :child-list '(1 2 3)))
+           (collect c)))))
+  (is (equal
+       '(1 2 3)
+       (serapeum:with-collectors (collect)
+         (do-child-slot-nodes (c (make 'node-list2 :body '(1 2 3)))
+           (collect c)))))
+  (is (every #'identity
+             (mapcar #'typep
+                     (serapeum:with-collectors (collect)
+                       (do-child-slot-nodes
+                           (c (make 'node-with-fields
+                                    :a (make 'node-with-fields)
+                                    :b (make 'node-with-fields)))
+                         (collect c)))
+                     '(node-with-fields node-with-fields))))
+  (is (equal
+       '(1 2)
+       (serapeum:with-collectors (collect)
+         ;; NB :data is not a child slot.
+         (do-child-slot-nodes (c (make 'node-with-arity2 :a '(1 2) :data 3))
+           (collect c)))))
+  (is (equal
+       '(1 2 3 4)
+       (serapeum:with-collectors (collect)
+         (do-child-slot-nodes (c (make 'node-with-arity2/2
+                                       :a '(1 2)
+                                       :b '(3 4)))
+           (collect c)))))
+  (is (equal
+       '(1 2 3 4)
+       (serapeum:with-collectors (collect)
+         (do-child-slot-nodes (c (make 'js-block-statement
+                                       :js-body (list 1 2 3 4)))
+           (collect c))))))
